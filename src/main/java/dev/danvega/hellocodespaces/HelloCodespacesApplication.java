79f84a8cb1ff38423165cd1eaea40710f3259ed7
Marcelo Vidal <msvidal@gmail.com>
2023-03-04 20:19:39 -0300

    Initial commit

---
+++ src/main/java/dev/danvega/hellocodespaces/HelloCodespacesApplication.java
@@ -0,0 +1,13 @@
+package dev.danvega.hellocodespaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloCodespacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloCodespacesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/danvega/hellocodespaces/HelloCodespacesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.danvega.hellocodespaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloCodespacesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
